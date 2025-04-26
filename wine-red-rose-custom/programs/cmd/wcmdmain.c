1b0adc70198571c44f34318570eaa2eb4c09bae5
rboxeur <david@david-Latitude-E6540.(none)>
2025-04-26 10:37:53 +0200

    Wine-upstream: merged 2 bugs fixed 51599 52399 56730
    https://bugs.winehq.org/show_bug.cgi?id=51599
    (wine commit = d7edf3e651a2601d6086f49d3b2907e2321b8607)
    
    https://bugs.winehq.org/show_bug.cgi?id=52399
    (wine commit = f51b4d014e1291cd3a394c7819516e7d1fce75ac)
    
    https://bugs.winehq.org/show_bug.cgi?id=56730
    (wine commit = 7b2ff977739df25252d46552d2447af50c23040e)

--- wine-red-rose-custom/programs/cmd/wcmdmain.c
+++ wine-red-rose-custom/programs/cmd/wcmdmain.c
@@ -2392,6 +2392,7 @@ WCHAR *WCMD_ReadAndParseLine(const WCHAR *optionalcmd, CMD_LIST **output, HANDLE
             } else break;
           }
 
+          extraData = WCMD_skip_leading_spaces(extraData);
         } while (*extraData == 0x00);
         curPos = extraSpace;
 
